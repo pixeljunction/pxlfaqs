6f2e3242f62b0780a8b732f99dd0a30294a60666
Mark Wilkinson <mark@markwilkinson.me>
2013-09-29 14:21:35 +0100

    Cleaned up the Default Archive Template

--- templates/archive-pxlfaq_faq.php
+++ templates/archive-pxlfaq_faq.php
@@ -1,85 +1,60 @@
 <?php
 
 /*
-* This is the main index template of this theme. The fallback
-* template used when others are not included. This is needed
-* for the theme to work.
+* FAQ archive template
 
 * lets start by getting the header template file
+* Important that the post title is wrapped with .accordion-header
+* and that the post content is wrapped with .accordion-content
+* classes.
 */
 
 get_header();
-
-?>
-
-	<div class="page-title-wrapper">
-	
-		<div class="container">
 		
-			<h1 class="page-title">Antiques</h1>
+	/* get the sidebar template for this content */
+	get_sidebar( 'faq' );
+	
+	?>
+	
+	<div class="content">
+
+	<?php
 		
-		</div>
-	
-	</div>
-	
-	<div class="container page-wrapper">
-	
-		<?php
+		/* check we have posts to display */
+		if( have_posts() ) {
 			
-			/* get the sidebar template for this content */
-			get_sidebar( 'antique' );
-		
-		?>
-		
-		<div class="col col-threequarter col-last content">
-
-		<?php
-		
-			/* check whether the breadcrumb is available */
-			if( function_exists('yoast_breadcrumb') ) {
-				
-				/* output the breadcrumb */
-				yoast_breadcrumb( '<p class="breadcrumb">', '</p>' );
+			/* begin to loop through each post */
+			while( have_posts() ) : the_post();
 			
-			}
-			
-			/* check we have posts to display */
-			if( have_posts() ) {
+				?>
 				
-				/* begin to loop through each post */
-				while( have_posts() ) : the_post();
-				
-					?>
-					
-					<article id="<?php echo get_post_type( $post->ID ); ?>-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<article id="<?php echo get_post_type( $post->ID ); ?>-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-						<h2 class="post-title accordion-header"><?php the_title(); ?></h2>
-						
-						<div class="entry-content accordion-content">
-						
-							<?php the_content(); ?>
-						
-						</div><!-- // entry-content -->
+					<h2 class="post-title accordion-header"><?php the_title(); ?></h2>
 					
-					</article><!-- // post-class -->
+					<div class="entry-content accordion-content">
 					
-					<?php
+						<?php the_content(); ?>
+					
+					</div><!-- // entry-content -->
 				
-				/* end the loop through each post */
-				endwhile;
+				</article><!-- // post-class -->
 				
-				/* output post navigation */
-				echo pxlcore_content_nav();
-				
-			} // end if have posts
-		
-		?>
+				<?php
+			
+			/* end the loop through each post */
+			endwhile;
+			
+			/* output post navigation */
+			echo pxlcore_content_nav();
+			
+		} // end if have posts
 	
-		</div><!-- // content -->
-		
-		<div class="clearfix"></div>
+	?>
+
+	</div><!-- // content -->
 	
-	</div><!-- // page-wrapper -->
+	<div class="clearfix"></div>
 
 <?php
 
